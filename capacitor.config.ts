35d473f1446b77f031b8153b9e0cba0a7486cc9a
Fabby24 <musaufabian7@gmail.com>
2026-03-27 15:03:18 +0300

    changes

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fabian.securelabwifi',
+  appName: 'SecureLab Wifi',
+  webDir: 'dist'
+};
+
+export default config;
